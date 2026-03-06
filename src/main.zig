92101cfa9b8351f89c233c8b2dcb35f82ec03477
rawson <110342737+rawsonb@users.noreply.github.com>
2026-03-05 21:29:43 -0500

    rocks :)

--- src/main.zig
+++ src/main.zig
@@ -3,14 +3,14 @@ const rl = @cImport({
     @cInclude("raylib.h");
 });
 
-const TILE_SIZE = 5;
+const TILE_SIZE = 8;
 const GRID_HEIGHT = 100;
 const GRID_WIDTH = 100;
 
 const ecs = @import("zflecs");
 
 const Position = struct { x: i32, y: i32 };
-const Visual = rl.Color;
+const Visual = rl.Texture2D;
 
 fn registerComponents(world: *ecs.world_t) void {
     ecs.COMPONENT(world, Position);
@@ -19,18 +19,9 @@ fn registerComponents(world: *ecs.world_t) void {
 
 fn renderVisuals(positions: []Position, visuals: []Visual) void {
     for (positions, visuals) |p, v| {
-        rl.DrawRectangle(p.x, p.y, TILE_SIZE, TILE_SIZE, v);
+        rl.DrawTexture(v, p.x, p.y, rl.WHITE);
     }
 }
-
-fn progressHues(it: *ecs.iter_t, visuals: []Visual) void {
-    for (visuals) |*v| {
-        var hsv = rl.ColorToHSV(v.*);
-        hsv.x += it.delta_time * 100.0;
-        v.* = rl.ColorFromHSV(hsv.x, hsv.y, hsv.z);
-    }
-}
-
 pub fn main() !void {
     rl.InitWindow(800, 450, "rib");
     defer rl.CloseWindow();
@@ -52,16 +43,21 @@ pub fn main() !void {
 
 fn registerSystems(world: *ecs.world_t) void {
     _ = ecs.ADD_SYSTEM(world, "render visuals", ecs.OnUpdate, renderVisuals);
-    _ = ecs.ADD_SYSTEM(world, "progress hues", ecs.OnUpdate, progressHues);
 }
 
 fn setupGame(world: *ecs.world_t) void {
+    const image = rl.LoadImage("assets/tiles/rock.png");
+    const texture = rl.LoadTextureFromImage(image);
     for (0..GRID_WIDTH) |xi| {
         for (0..GRID_HEIGHT) |yi| {
             const testing = ecs.new_id(world);
             _ = ecs.set(world, testing, Position, .{ .x = @intCast(xi * TILE_SIZE), .y = @intCast(yi * TILE_SIZE) });
-            const hue = @rem(@as(f32, @floatFromInt(yi)) * @as(f32, @floatFromInt(xi)), 360.0);
-            _ = ecs.set(world, testing, Visual, rl.ColorFromHSV(hue, 1.0, 1.0));
+            _ = ecs.set(
+                world,
+                testing,
+                Visual,
+                texture,
+            );
         }
     }
 }
